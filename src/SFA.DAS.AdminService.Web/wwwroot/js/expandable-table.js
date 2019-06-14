946e58987452e38ca0815f9978ca59bbaa463a33
ArshedMust <arshed@air2wire.com>
2019-06-14 17:51:40 +0100

    Updated so the options can be deselected independently but still when selected work mutually exclusively

--- src/SFA.DAS.AdminService.Web/wwwroot/js/expandable-table.js
+++ src/SFA.DAS.AdminService.Web/wwwroot/js/expandable-table.js
@@ -49,6 +49,9 @@
 
         $(".js-expand-table-row-for-radio").each(function () {
             var ariaControlId = "table-content-" + $(this).data("expand-id");
+            var $optionContainer = $(this).parent().parent()[0];
+            var $firstOption = $optionContainer.firstElementChild;
+            var $lastOption = $optionContainer.lastElementChild;
             var $expandable = $(this)
                 .closest("tr")
                 .nextAll("tr.js-expandble-cell-for-radio:first");
@@ -62,6 +65,44 @@
             // show and hide based on click and update aria tags
             $(this).on("click keypress", function (event) {
                 if (event.type === "keypress" && event.keyCode !== 13) return;
+                if (event.currentTarget.classList.contains("js-first-option")) {
+                    //Clear other option in case it is marked checked
+                    if ($lastOption.firstElementChild.classList.contains("checked")) {
+                        $lastOption.firstElementChild.checked = false;
+                        $lastOption.firstElementChild.classList.remove("checked");
+                    }
+                    //If current option is checked then clear 
+                    if (event.currentTarget.classList.contains("checked")) {
+                        $firstOption.firstElementChild.checked = false;
+                        event.currentTarget.classList.remove("checked");
+                    }
+                    else {
+                        //Current option was not checked so mark checked
+                        $firstOption.firstElementChild.checked = true;
+                        event.currentTarget.classList.add("checked");
+                    }
+                } else {
+                    //Clear other option in case it is marked checked
+                    if ($firstOption.firstElementChild.classList.contains("checked")) {
+                        $firstOption.firstElementChild.checked = false;
+                        $firstOption.firstElementChild.classList.remove("checked");
+                    }
+                    //If current option is checked then clear option just hide reason in case it is showing
+                    if (event.currentTarget.classList.contains("checked")) {
+                        $lastOption.firstElementChild.checked = false;
+                        event.currentTarget.classList.remove("checked");
+                        $(this).attr("aria-expanded", "false");
+                        $expandable
+                            .addClass("js-hidden")
+                            .attr({ "aria-hidden": "true", id: ariaControlId });
+                        return;
+                    }
+                    else {
+                        //Current option was not checked so mark checked
+                        $lastOption.firstElementChild.checked = true;
+                        event.currentTarget.classList.add("checked");
+                    }
+                }
                 if ($(this).attr("data-hide")) {
                     //FORCE HIDE CONTENT
                     $(this).attr("aria-expanded", "false");
