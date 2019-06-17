14198ef1a2dcf1a6179f5307a8f87aacc463eaf2
ArshedMust <arshed@air2wire.com>
2019-06-17 13:43:08 +0100

    Check options working on IE and cleaned up code

--- src/SFA.DAS.AdminService.Web/wwwroot/js/expandable-table.js
+++ src/SFA.DAS.AdminService.Web/wwwroot/js/expandable-table.js
@@ -3,8 +3,8 @@
 
   var $ = global.jQuery;
   var GOVUK = global.GOVUK || {};
-
-  GOVUK.expandableTable = {
+  
+ var obj = GOVUK.expandableTable = {
     init: function init() {
       // loop over table row and add relevant attributes
       $(".js-expand-table-row").each(function() {
@@ -36,10 +36,7 @@
               .text("\u25bc");
           } else {
             // HIDE CONTENT
-            $(this).attr("aria-expanded", "false");
-            $expandable
-              .addClass("js-hidden")
-              .attr({ "aria-hidden": "true", id: ariaControlId });
+              obj.hideContent($(this), ariaControlId, $expandable);
             $arrow
               .attr({ class: "arrow arrow-closed", "aria-hidden": "true" })
               .text("\u25ba");
@@ -47,16 +44,12 @@
         });
         });
 
-        $(".js-expand-table-row-for-radio").each(function () {
+          $(".js-expand-table-row-for-radio").each(function () {
             var ariaControlId = "table-content-" + $(this).data("expand-id");
-            var $optionContainer = $(this).parent().parent()[0];
-            var $firstOption = $optionContainer.firstElementChild;
-            var $lastOption = $optionContainer.lastElementChild;
             var $expandable = $(this)
                 .closest("tr")
                 .nextAll("tr.js-expandble-cell-for-radio:first");
            
-            var $notRejected = $(this).find("aria-labelledby");
             $(this).attr({
                 "aria-expanded": "false",
                 "aria-controls": ariaControlId
@@ -64,51 +57,40 @@
             $expandable.attr({ "aria-hidden": "true", id: ariaControlId });
             // show and hide based on click and update aria tags
             $(this).on("click keypress", function (event) {
-                if (event.type === "keypress" && event.keyCode !== 13) return;
+                if (event.type === "keypress" && event.keyCode !== 13) return
+                var $optionContainer = $(this).parent().parent()[0];
+                var $firstOption = $optionContainer.firstElementChild;
+                var $lastOption = $optionContainer.lastElementChild;
                 if (event.currentTarget.classList.contains("js-first-option")) {
-                    //Clear other option in case it is marked checked
-                    if ($lastOption.firstElementChild.classList.contains("checked")) {
-                        $lastOption.firstElementChild.checked = false;
-                        $lastOption.firstElementChild.classList.remove("checked");
-                    }
+                     //Clear other option in case it is marked checked
+                    obj.clearOtherOption($lastOption);
                     //If current option is checked then clear 
                     if (event.currentTarget.classList.contains("checked")) {
-                        $firstOption.firstElementChild.checked = false;
-                        event.currentTarget.classList.remove("checked");
+                        obj.clearOption($firstOption, event);
+                        obj.clearHiddenValue($firstOption);
                     }
                     else {
                         //Current option was not checked so mark checked
-                        $firstOption.firstElementChild.checked = true;
-                        event.currentTarget.classList.add("checked");
+                        obj.checkOption($firstOption, event);
                     }
                 } else {
                     //Clear other option in case it is marked checked
-                    if ($firstOption.firstElementChild.classList.contains("checked")) {
-                        $firstOption.firstElementChild.checked = false;
-                        $firstOption.firstElementChild.classList.remove("checked");
-                    }
+                    obj.clearOtherOption($firstOption);
                     //If current option is checked then clear option just hide reason in case it is showing
                     if (event.currentTarget.classList.contains("checked")) {
-                        $lastOption.firstElementChild.checked = false;
-                        event.currentTarget.classList.remove("checked");
-                        $(this).attr("aria-expanded", "false");
-                        $expandable
-                            .addClass("js-hidden")
-                            .attr({ "aria-hidden": "true", id: ariaControlId });
+                        obj.clearOption($lastOption, event);
+                        obj.clearHiddenValue($lastOption);
+                        obj.hideContent($(this), ariaControlId, $expandable);
                         return;
                     }
                     else {
                         //Current option was not checked so mark checked
-                        $lastOption.firstElementChild.checked = true;
-                        event.currentTarget.classList.add("checked");
+                        obj.checkOption($lastOption, event);
                     }
                 }
                 if ($(this).attr("data-hide")) {
                     //FORCE HIDE CONTENT
-                    $(this).attr("aria-expanded", "false");
-                    $expandable
-                        .addClass("js-hidden")
-                        .attr({ "aria-hidden": "true", id: ariaControlId });
+                    obj.hideContent($(this), ariaControlId, $expandable);
                 }
                 else if ($expandable.hasClass("js-hidden")) {
                     // SHOW CONTENT
@@ -121,8 +103,34 @@
                 } 
             });
         });
+      },
+      clearHiddenValue: function ($option) {
+          if ($option.firstElementChild.getAttribute('data-clear-on-uncheck')) {
+              $option.firstElementChild.value = "";
+              $("input[name='" + $option.firstElementChild.name + "'][type='hidden']").val('');
+          }
+      },
+     hideContent: function ($element, ariaControlId, $expandable) {
+          $element.attr("aria-expanded", "false");
+          $expandable
+              .addClass("js-hidden")
+              .attr({ "aria-hidden": "true", id: ariaControlId });
+      },
+      clearOtherOption: function ($option) {
+          if ($option.firstElementChild.classList.contains("checked")) {
+              $option.firstElementChild.checked = false;
+              $option.firstElementChild.classList.remove("checked");
+          }
+      },
+      checkOption: function ($option, event) {
+          $option.firstElementChild.checked = true;
+          event.currentTarget.classList.add("checked");
+      },
+      clearOption: function ($option, event) {
+          $option.firstElementChild.checked = false;
+          event.currentTarget.classList.remove("checked");
+      }
 
-    }
   };
 
   global.GOVUK = GOVUK;
