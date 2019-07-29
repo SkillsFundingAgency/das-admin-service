5f4241dedaea8a050885e9591f281c21830076f7
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-07-29 12:09:44 +0100

    ON-2134 - This will be the job of assessor service

--- src/SFA.DAS.AdminService.Application.Interfaces/IEpaOrganisationSearchValidator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.AdminService.Application.Interfaces
-{
-    public interface IEpaOrganisationSearchValidator
-    {
-        bool IsValidEpaOrganisationId(string organisationIdToCheck);
-        bool IsValidUkprn(string stringToCheck);
-    }
-}
