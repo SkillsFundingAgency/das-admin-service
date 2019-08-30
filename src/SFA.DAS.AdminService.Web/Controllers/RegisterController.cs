b7d434443116eba4e758f5495fe279cffc85a12a
Chris Woodcock <chris@humesoftware.com>
2019-08-30 17:48:23 +0100

    Pass operator name to organisation and contact updates for audit

--- src/SFA.DAS.AdminService.Web/Controllers/RegisterController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/RegisterController.cs
@@ -101,7 +101,8 @@ namespace SFA.DAS.AdminService.Web.Controllers
                 CompanyNumber = viewModel.CompanyNumber,
                 CharityNumber = viewModel.CharityNumber,
                 FinancialDueDate = viewModel.FinancialDueDate,
-                FinancialExempt = viewModel.FinancialExempt
+                FinancialExempt = viewModel.FinancialExempt,
+                UpdatedBy = HttpContext.User.OperatorName()
             };
          
             await _apiClient.UpdateEpaOrganisation(updateOrganisationRequest);
@@ -269,7 +270,8 @@ namespace SFA.DAS.AdminService.Web.Controllers
                 LastName = viewAndEditModel.LastName,
                 Email = viewAndEditModel.Email,
                 PhoneNumber = viewAndEditModel.PhoneNumber,
-                ActionChoice = viewAndEditModel.ActionChoice
+                MakePrimaryContact = viewAndEditModel.ActionChoice.Equals("MakePrimaryContact"),
+                UpdatedBy = HttpContext.User.OperatorName()
             };
             await _apiClient.UpdateEpaContact(request);
             return RedirectToAction("ViewContact", "register", new { contactId = viewAndEditModel.ContactId});
