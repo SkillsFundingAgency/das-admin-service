b1b076ceb0e2f1fbd1f146cb8df6e8a635965d23
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-10-01 16:31:11 +0100

    ON-2307 - Updated FHA dashboards based on new status codes

---
+++ src/SFA.DAS.AssessorService.ApplyTypes/FinancialReviewStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.AssessorService.ApplyTypes
+{
+    public class FinancialReviewStatus
+    {
+        public const string Required = "Required";
+        public const string Exempt = "Exempt";
+
+        public const string New = "New";
+        public const string InProgress = "In Progress";
+        public const string Rejected = "Rejected";
+        public const string Closed = "Closed";
+    }
+}
