44efa502a389136967fc06f99cf32eb9dc1c7fcf
Chris Jensen <chrisjensenuk@gmail.com>
2019-10-17 08:18:20 +0100

    Roatp Assessor Dashboard

---
+++ src/SFA.DAS.AdminService.Web/Controllers/RoatpAssessor/RoatpAssessorDashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AdminService.Web.Controllers.RoatpAssessor
+{
+    [Authorize(Roles = Domain.Roles.RoatpAssessorTeam)]
+    public class RoatpAssessorDashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("~/Views/RoatpAssessor/RoatpAssessorDashboard/Index.cshtml");
+        }
+    }
+}
