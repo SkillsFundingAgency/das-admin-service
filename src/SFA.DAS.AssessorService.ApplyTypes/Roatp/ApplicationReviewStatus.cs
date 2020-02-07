5b5b01bdd3e36d54fb6cffb4f41ae374d59d83ee
Martin Lush <martin-hornsby@ntlworld.com>
2020-02-07 12:09:08 +0000

    APR-1269 - Added RoATP Review Status classes

---
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/ApplicationReviewStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
+{
+    public static class ApplicationReviewStatus
+    {
+        public const string Draft = "Draft";
+        public const string New = "New";
+        public const string InProgress = "In Progress";
+        public const string HasFeedback = "Has Feedback";
+        public const string Approved = "Approved";
+        public const string Declined = "Declined";
+    }
+}
