8cecddf9045608987301dd7fb8d6102f3bd04467
Martin Lush <martin-hornsby@ntlworld.com>
2020-02-19 11:41:04 +0000

    Sync'ed status consts for RoATP

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/ApplicationStatus.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/ApplicationStatus.cs
@@ -5,10 +5,11 @@
         public const string New = "New";
         public const string InProgress = "In Progress";
         public const string Submitted = "Submitted";
-        public const string FeedbackAdded = "FeedbackAdded";
         public const string Resubmitted = "Resubmitted";
-        public const string Declined = "Declined";
-        public const string Approved = "Approved";
         public const string GatewayAssessed = "GatewayAssessed";
+        public const string FeedbackAdded = "FeedbackAdded";
+        public const string Rejected = "Rejected";
+        public const string Approved = "Approved";
+        public const string Cancelled = "Cancelled";
     }
 }
--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/AssessorReviewStatus.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/AssessorReviewStatus.cs
@@ -5,7 +5,9 @@
         public const string Draft = "Draft";
         public const string New = "New";
         public const string InProgress = "In Progress";
+        public const string HasFeedback = "Has Feedback";
         public const string Approved = "Approved";
         public const string Declined = "Declined";
+        public const string NotRequired = "Not Required";
     }
 }
