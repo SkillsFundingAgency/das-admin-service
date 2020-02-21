dbf2a239c61615d5a25af5301fc4c70ae8770ed8
Dave Read <david.read@digital.education.gov.uk>
2020-02-21 13:41:57 +0000

    APR-1271 sync Apply API type for provider route

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/Apply.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/Apply.cs
@@ -64,7 +64,7 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
         public string UKPRN { get; set; }
         public string OrganisationName { get; set; }
         public string TradingName { get; set; }
-        public string ProviderRoute { get; set; }
+        public int ProviderRoute { get; set; }
         public string ProviderRouteName { get; set; }
         public DateTime? ApplicationSubmittedOn { get; set; }
         public Guid? ApplicationSubmittedBy { get; set; }
