89e16b018710fb65569b27a7aab159cf1f5740a5
Anatoli Malev <makcom@hotmail.co.uk>
2020-03-10 15:26:39 +0000

    changing static to const

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/GatewayPageIds.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/GatewayPageIds.cs
@@ -6,15 +6,15 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
 {
     public static class GatewayPageIds
     {
-        public static string LegalName => "1-10";
-        public static string TradingName => "1-20";
+        public const string LegalName = "1-10";
+        public const string TradingName = "1-20";
 
-        public static string WebsiteAddress => "1-60";
+        public const string WebsiteAddress = "1-60";
 
 
-        public static string OfficeForStudent => "4-10";
-        public static string InitialTeacherTraining => "4-20";
-        public static string Ofsted => "4-30";
-        public static string SubcontractorDeclaration => "4-40";
+        public const string OfficeForStudent = "4-10";
+        public const string InitialTeacherTraining = "4-20";
+        public const string Ofsted = "4-30";
+        public const string SubcontractorDeclaration = "4-40";
     }
 }
