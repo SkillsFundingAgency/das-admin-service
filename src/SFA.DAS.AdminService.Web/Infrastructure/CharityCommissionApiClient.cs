0ff185a2b90fb29ccf529cb32483865de716c998
MarkFCain <mark.cain@digital.education.gov.uk>
2020-03-10 11:41:35 +0000

    APR-1283 final finessing

--- src/SFA.DAS.AdminService.Web/Infrastructure/CharityCommissionApiClient.cs
+++ src/SFA.DAS.AdminService.Web/Infrastructure/CharityCommissionApiClient.cs
@@ -34,38 +34,6 @@ namespace SFA.DAS.AdminService.Web.Infrastructure
                 await _httpClient.GetAsync($"/charity-commission-lookup?charityNumber={charityNumber}");
 
             return await requestMessage.Content.ReadAsAsync<Charity>();
-            //return await Get<Charity>($"/charity-commission-lookup?charityNumber={charityNumber}");
         }
     }
-
-
-
-    //public class ApplyServicePassthroughApiClient : IApplyServicePassthroughApiClient
-    //{
-    //    private readonly HttpClient _client;
-    //    private readonly IRoatpApplyTokenService _tokenService;
-
-    //    public ApplyServicePassthroughApiClient(string baseUri, IRoatpApplyTokenService tokenService)
-    //    {
-    //        _client = new HttpClient { BaseAddress = new Uri(baseUri) };
-    //        _tokenService = tokenService;
-    //    }
-
-    //    public async Task<Charity> GetCharityDetails(string charityNumber)
-    //    {
-    //        return await Get<Charity>($"/charity-commission-lookup?charityNumber={charityNumber}");
-    //    }
-
-
-    //    private async Task<T> Get<T>(string uri)
-    //    {
-    //        _client.DefaultRequestHeaders.Authorization =
-    //            new AuthenticationHeaderValue("Bearer", _tokenService.GetToken());
-
-    //        using (var response = await _client.GetAsync(new Uri(uri, UriKind.Relative)))
-    //        {
-    //            return await response.Content.ReadAsAsync<T>();
-    //        }
-    //    }
-    //}
 }
