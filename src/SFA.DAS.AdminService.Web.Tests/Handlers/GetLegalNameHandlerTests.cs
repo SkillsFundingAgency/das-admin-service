a20d70dbc30b084b0e794134a0c18aaed57c09e9
MarkFCain <mark.cain@digital.education.gov.uk>
2020-03-12 09:11:30 +0000

    APR-1291 out for TR

---
+++ src/SFA.DAS.AdminService.Web.Tests/Handlers/GetLegalNameHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SFA.DAS.AdminService.Web.Handlers.Gateway;
+
+namespace SFA.DAS.AdminService.Web.Tests.Handlers
+{
+    [TestFixture]
+    public class GetLegalNameHandlerTests
+    {
+        private GetLegalNameHandler _handler;
+        //private Mock<IRoatpApplicationApiClient> _applyApiClient;
+        //private Mock<IHttpContextAccessor> _contextAccessor;
+        //private Mock<IRoatpGatewayPageViewModelValidator> _gatewayValidator;
+    }
+}
