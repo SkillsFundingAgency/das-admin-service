058d9d452b348dede0e907bd198b9871c023ffdc
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-13 09:00:21 +0000

    APR-1477 - Added ApplicationRoute to RoatpApplicationSummaryItem

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/RoatpApplicationSummaryItem.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/RoatpApplicationSummaryItem.cs
@@ -6,6 +6,7 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
     {
         public Guid Id { get; set; }
         public Guid ApplicationId { get; set; }
+        public string ApplicationRoute { get; set; }
         public string OrganisationName { get; set; }
         public string Ukprn { get; set; }
         public string ApplicationReferenceNumber { get; set; }
@@ -16,5 +17,22 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
         public string GatewayReviewStatus { get; set; }
         public string AssessorReviewStatus { get; set; }
         public string FinancialReviewStatus { get; set; }
+
+        public string ApplicationRouteShortText
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(ApplicationRoute))
+                {
+                    return string.Empty;
+                }
+                var index = ApplicationRoute.IndexOf(' ');
+                if (index < 0)
+                {
+                    return ApplicationRoute;
+                }
+                return ApplicationRoute.Substring(0, index + 1);
+            }
+        }
     }
 }
