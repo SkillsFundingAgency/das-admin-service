e98c9c752a4401ead6c62d7ce1f2e9993c91bf03
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-23 12:56:57 +0000

    APR-1477 - Added DeclaredInApplication field

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/RoatpFinancialSummaryItem.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/RoatpFinancialSummaryItem.cs
@@ -3,6 +3,8 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
 {
     public class RoatpFinancialSummaryItem : RoatpApplicationSummaryItem
     {
+        public string DeclaredInApplication { get; set; }
+
         public FinancialReviewDetails FinancialReviewDetails { get; set; }
     }
 }
