9fd0ebda7c6e7822498318a39d600d6eff291077
Michael Young <mick.young1981@googlemail.com>
2020-03-27 11:44:04 +0000

    Added subcontractor declaration controller method.

---
+++ src/SFA.DAS.AdminService.Web.Tests/Controllers/Gateway/ExperienceAndAccreditation/SubcontractorDeclarationTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AdminService.Web.Controllers.Roatp.Apply;
+using SFA.DAS.AdminService.Web.Infrastructure.RoatpClients;
+using SFA.DAS.AdminService.Web.Services.Gateway;
+using SFA.DAS.AdminService.Web.Validators.Roatp;
+using SFA.DAS.AdminService.Web.ViewModels.Roatp.Gateway;
+using SFA.DAS.AssessorService.Api.Types.Models.Validation;
+using SFA.DAS.AssessorService.ApplyTypes.Roatp;
+
+namespace SFA.DAS.AdminService.Web.Tests.Controllers.Gateway.ExperienceAndAccreditation
+{
+    [TestFixture]
+    public class SubcontractorDeclarationTests
+    {
+        private RoatpGatewayExperienceAndAccreditationController _controller;
+        private Mock<IRoatpApplicationApiClient> _applyApiClient;
+        private Mock<IHttpContextAccessor> _contextAccessor;
+        private Mock<IRoatpGatewayPageViewModelValidator> _gatewayValidator;
+        private Mock<IGatewayExperienceAndAccreditationOrchestrator> _orchestrator;
+        private Mock<ILogger<RoatpGatewayExperienceAndAccreditationController>> _logger;
+
+        private string username => "user name";
+        private string givenName => "user";
+        private string surname => "name";
+
+        [SetUp]
+        public void Setup()
+        {
+            _applyApiClient = new Mock<IRoatpApplicationApiClient>();
+            _contextAccessor = new Mock<IHttpContextAccessor>();
+            _gatewayValidator = new Mock<IRoatpGatewayPageViewModelValidator>();
+            _logger = new Mock<ILogger<RoatpGatewayExperienceAndAccreditationController>>();
+            _orchestrator = new Mock<IGatewayExperienceAndAccreditationOrchestrator>();
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn", username),
+                new Claim(ClaimTypes.GivenName, givenName),
+                new Claim(ClaimTypes.Surname, surname)
+            }));
+
+            var context = new DefaultHttpContext { User = user };
+            _gatewayValidator.Setup(v => v.Validate(It.IsAny<LegalNamePageViewModel>()))
+                .ReturnsAsync(new ValidationResponse
+                    {
+                        Errors = new List<ValidationErrorDetail>()
+                    }
+                );
+            _contextAccessor.Setup(_ => _.HttpContext).Returns(context);
+
+            _controller = new RoatpGatewayExperienceAndAccreditationController(_applyApiClient.Object, _contextAccessor.Object, _gatewayValidator.Object, _orchestrator.Object, _logger.Object);
+        }
+
+        [Test]
+        public void subcontractor_declaration_is_returned()
+        {
+            var applicationId = Guid.NewGuid();
+            var expectedViewModel = new SubcontractorDeclarationViewModel();
+            
+            _orchestrator.Setup(x => x.GetSubcontractorDeclarationViewModel(It.Is<GetSubcontractorDeclarationRequest>(y => y.ApplicationId == applicationId && y.UserName == username))).ReturnsAsync(expectedViewModel);
+                
+            var result = _controller.SubcontractorDeclaration(applicationId).Result;
+            Assert.AreSame(expectedViewModel, result.Model);
+        }
+    }
+}
--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpGatewayExperienceAndAccreditationController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpGatewayExperienceAndAccreditationController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AdminService.Web.Domain;
+using SFA.DAS.AdminService.Web.Infrastructure.RoatpClients;
+using SFA.DAS.AdminService.Web.Services.Gateway;
+using SFA.DAS.AdminService.Web.Validators.Roatp;
+using SFA.DAS.AssessorService.ApplyTypes.Roatp;
 
 namespace SFA.DAS.AdminService.Web.Controllers.Roatp.Apply
 {
-    public class RoatpGatewayExperienceAndAccreditationController : Controller
+    [Authorize(Roles = Roles.RoatpGatewayTeam + "," + Roles.CertificationTeam)]
+    public class RoatpGatewayExperienceAndAccreditationController : RoatpGatewayControllerBase
     {
-        public IActionResult SubcontractorDeclaration()
+        private readonly IRoatpApplicationApiClient _roatpApiClient;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IRoatpGatewayPageViewModelValidator _validator;
+        private readonly IGatewayExperienceAndAccreditationOrchestrator _orchestrator;
+        private readonly ILogger<RoatpGatewayExperienceAndAccreditationController> _logger;
+
+        public RoatpGatewayExperienceAndAccreditationController(IRoatpApplicationApiClient roatpApiClient, IHttpContextAccessor contextAccessor, IRoatpGatewayPageViewModelValidator validator, IGatewayExperienceAndAccreditationOrchestrator orchestrator, ILogger<RoatpGatewayExperienceAndAccreditationController> logger)
         {
-            return View();
+            _roatpApiClient = roatpApiClient;
+            _contextAccessor = contextAccessor;
+            _validator = validator;
+            _orchestrator = orchestrator;
+            _logger = logger;
+        }
+
+        public async Task<ViewResult> SubcontractorDeclaration(Guid applicationId)
+        {
+            var userName = _contextAccessor.HttpContext.User.UserDisplayName();
+            var viewModel = await _orchestrator.GetSubcontractorDeclarationViewModel(new GetSubcontractorDeclarationRequest(applicationId, userName));
+            return View(viewModel);
         }
     }
 }
