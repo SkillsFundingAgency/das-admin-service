dcf3749dd736e734a2118829d8bde02f3a1a92d3
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-30 14:28:28 +0100

    APR-1294 - Added RoatpGatewayRegisterChecksController and unit tests

---
+++ src/SFA.DAS.AdminService.Web.Tests/Controllers/Gateway/RegisterChecks/RoatpTests.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.AdminService.Web.Controllers.Roatp.Apply;
+using SFA.DAS.AdminService.Web.Infrastructure;
+using SFA.DAS.AdminService.Web.Services.Gateway;
+using SFA.DAS.AdminService.Web.Validators.Roatp;
+using SFA.DAS.AdminService.Web.ViewModels.Roatp.Gateway;
+using SFA.DAS.AssessorService.Api.Types.Models.Validation;
+using SFA.DAS.AssessorService.ApplyTypes.Roatp;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AdminService.Web.Tests.Controllers.Gateway.RegisterChecks
+{
+    [TestFixture]
+    public class RoatpTests
+    {
+        private RoatpGatewayRegisterChecksController _controller;
+        private Mock<IRoatpApplicationApiClient> _applyApiClient;
+        private Mock<IHttpContextAccessor> _contextAccessor;
+        private Mock<IRoatpGatewayPageViewModelValidator> _gatewayValidator;
+        private Mock<IGatewayRegisterChecksOrchestrator> _orchestrator;
+        private Mock<ILogger<RoatpGatewayRegisterChecksController>> _logger;
+
+        private string username = "john smith";
+        private string givenName = "john";
+        private string surname = "smith";
+
+        [SetUp]
+        public void Setup()
+        {
+            _applyApiClient = new Mock<IRoatpApplicationApiClient>();
+            _contextAccessor = new Mock<IHttpContextAccessor>();
+            _gatewayValidator = new Mock<IRoatpGatewayPageViewModelValidator>();
+            _logger = new Mock<ILogger<RoatpGatewayRegisterChecksController>>();
+            _orchestrator = new Mock<IGatewayRegisterChecksOrchestrator>();
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+             {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn", username),
+                new Claim(ClaimTypes.GivenName, givenName),
+                new Claim(ClaimTypes.Surname, surname)
+             }));
+
+            _contextAccessor.Setup(_ => _.HttpContext).Returns(new DefaultHttpContext { User = user });
+
+            _gatewayValidator.Setup(v => v.Validate(It.IsAny<RoatpPageViewModel>()))
+                .ReturnsAsync(new ValidationResponse
+                {
+                    Errors = new List<ValidationErrorDetail>()
+                });
+
+            _controller = new RoatpGatewayRegisterChecksController(_applyApiClient.Object, _contextAccessor.Object, _gatewayValidator.Object, _orchestrator.Object, _logger.Object);
+        }
+
+        [Test]
+        public async Task check_roatp_request_is_sent()
+        {
+            var applicationId = Guid.NewGuid();
+
+            _orchestrator.Setup(x => x.GetRoatpViewModel(new GetRoatpRequest(applicationId, username)))
+                .ReturnsAsync(new RoatpPageViewModel())
+                .Verifiable("view model not returned");
+
+            var _result = await _controller.GetGatewayRoatpPage(applicationId, GatewayPageIds.Roatp);
+            _orchestrator.Verify(x => x.GetRoatpViewModel(It.Is<GetRoatpRequest>(y => y.ApplicationId == applicationId && y.UserName == username)), Times.Once());
+        }
+
+        [Test]
+        public async Task roatp_viewmodel_is_returned()
+        {
+            var applicationId = Guid.NewGuid();
+            var expectedViewModel = new RoatpPageViewModel();
+
+            _orchestrator.Setup(x => x.GetRoatpViewModel(It.Is<GetRoatpRequest>(y => y.ApplicationId == applicationId && y.UserName == username))).ReturnsAsync(expectedViewModel);
+
+            var _result = await _controller.GetGatewayRoatpPage(applicationId, GatewayPageIds.Roatp);
+            var viewModel = (_result as ViewResult)?.ViewData.Model as RoatpPageViewModel;
+
+            Assert.AreSame(expectedViewModel, viewModel);
+        }
+
+        [Test]
+        public async Task saving_roatp_happy_path_saves_evaluation_result()
+        {
+            var applicationId = Guid.NewGuid();
+            var pageId = GatewayPageIds.Roatp;
+
+            var vm = new RoatpPageViewModel
+            {
+                ApplicationId = applicationId,
+                PageId = pageId,
+                Status = SectionReviewStatus.Pass,
+                SourcesCheckedOn = DateTime.Now,
+                ErrorMessages = new List<ValidationErrorDetail>(),
+                OptionPassText = "Some pass text"
+            };
+
+            _gatewayValidator.Setup(v => v.Validate(vm)).ReturnsAsync(new ValidationResponse { Errors = new List<ValidationErrorDetail>() });
+
+            var result = await _controller.EvaluateRoatpPage(vm);
+
+            _applyApiClient.Verify(x => x.SubmitGatewayPageAnswer(applicationId, pageId, vm.Status, username, vm.OptionPassText), Times.Once);
+        }
+
+        [Test]
+        public async Task saving_roatp_without_required_fields_does_not_save()
+        {
+            var applicationId = Guid.NewGuid();
+            var pageId = GatewayPageIds.Roatp;
+
+            var vm = new RoatpPageViewModel
+            {
+                ApplicationId = applicationId,
+                PageId = pageId,
+                Status = SectionReviewStatus.Fail,
+                SourcesCheckedOn = DateTime.Now,
+                ErrorMessages = new List<ValidationErrorDetail>(),
+                OptionFailText = null
+            };
+
+            _gatewayValidator.Setup(v => v.Validate(It.IsAny<RoatpPageViewModel>()))
+                .ReturnsAsync(new ValidationResponse
+                {
+                    Errors = new List<ValidationErrorDetail>
+                        {
+                            new ValidationErrorDetail {Field = "OptionFail", ErrorMessage = "needs text"}
+                        }
+                });
+
+            var result = await _controller.EvaluateRoatpPage(vm);
+
+            _applyApiClient.Verify(x => x.SubmitGatewayPageAnswer(applicationId, pageId, vm.Status, username, vm.OptionPassText), Times.Never);
+        }
+    }
+}
---
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpGatewayRegisterChecksController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminService.Web.Domain;
+using SFA.DAS.AdminService.Web.Infrastructure;
+using SFA.DAS.AdminService.Web.ViewModels.Roatp.Gateway;
+using SFA.DAS.AssessorService.ApplyTypes.Roatp;
+using System;
+using System.Threading.Tasks;
+using SFA.DAS.AdminService.Web.Validators.Roatp;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AdminService.Web.Services.Gateway;
+
+namespace SFA.DAS.AdminService.Web.Controllers.Roatp.Apply
+{
+    [Authorize(Roles = Roles.RoatpGatewayTeam + "," + Roles.CertificationTeam)]
+    public class RoatpGatewayRegisterChecksController : RoatpGatewayControllerBase
+    {
+        private readonly IRoatpApplicationApiClient _applyApiClient;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IRoatpGatewayPageViewModelValidator _gatewayValidator;
+        private readonly ILogger<RoatpGatewayRegisterChecksController> _logger;
+        private readonly IGatewayRegisterChecksOrchestrator _orchestrator;
+
+        private const string GatewayViewsLocation = "~/Views/Roatp/Apply/Gateway/pages";
+
+        public RoatpGatewayRegisterChecksController(IRoatpApplicationApiClient applyApiClient,
+                                                IHttpContextAccessor contextAccessor,
+                                                IRoatpGatewayPageViewModelValidator gatewayValidator,
+                                                IGatewayRegisterChecksOrchestrator orchestrator,
+                                                ILogger<RoatpGatewayRegisterChecksController> logger) : base()
+        {
+            _applyApiClient = applyApiClient;
+            _contextAccessor = contextAccessor;
+            _gatewayValidator = gatewayValidator;
+            _logger = logger;
+            _orchestrator = orchestrator;
+        }
+
+        [HttpGet("/Roatp/Gateway/{applicationId}/Page/Roatp")]
+        public async Task<IActionResult> GetGatewayRoatpPage(Guid applicationId, string pageId)
+        {
+            var username = _contextAccessor.HttpContext.User.UserDisplayName();
+            var viewModel = await _orchestrator.GetRoatpViewModel(new GetRoatpRequest(applicationId, username));
+            return View($"{GatewayViewsLocation}/Roatp.cshtml", viewModel);
+        }
+
+        [HttpPost("/Roatp/Gateway/{applicationId}/Page/Roatp")]
+        public async Task<IActionResult> EvaluateRoatpPage(RoatpPageViewModel viewModel)
+        {
+
+            var validationResponse = await _gatewayValidator.Validate(viewModel);
+
+            if (validationResponse.Errors != null && validationResponse.Errors.Any())
+            {
+                viewModel.ErrorMessages = validationResponse.Errors;
+                return View($"{GatewayViewsLocation}/Roatp.cshtml", viewModel);
+            }
+
+            var username = _contextAccessor.HttpContext.User.UserDisplayName();
+            var comments = SetupGatewayPageOptionTexts(viewModel);
+
+            _logger.LogInformation($"RoatpGatewayRegisterChecksController-EvaluateRoatpPage-SubmitGatewayPageAnswer - ApplicationId '{viewModel.ApplicationId}' - PageId '{viewModel.PageId}' - Status '{viewModel.Status}' - UserName '{username}' - Comments '{comments}'");
+            try
+            {
+                await _applyApiClient.SubmitGatewayPageAnswer(viewModel.ApplicationId, viewModel.PageId, viewModel.Status, username, comments);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "RoatpGatewayRegisterChecksController-EvaluateRoatpPage-SubmitGatewayPageAnswer - Error: '" + ex.Message + "'");
+                throw;
+            }
+
+            return RedirectToAction("ViewApplication", "RoatpGateway", new { viewModel.ApplicationId });
+        }
+    }
+}
