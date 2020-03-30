6ed0f4a4bcd311797934c6c4d65bd28f7a859c1e
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-30 14:27:31 +0100

    APR-1294 - Implemented GatewayRegisterChecksOrchestrator and unit tests

---
+++ src/SFA.DAS.AdminService.Web/Services/Gateway/GatewayRegisterChecksOrchestrator.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Logging;
+using SFA.DAS.AdminService.Web.Infrastructure;
+using SFA.DAS.AdminService.Web.ViewModels.Roatp.Gateway;
+using SFA.DAS.AssessorService.ApplyTypes.Roatp;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.AdminService.Web.Services.Gateway
+{
+    public class GatewayRegisterChecksOrchestrator : IGatewayRegisterChecksOrchestrator
+    {
+        private readonly IRoatpApplicationApiClient _applyApiClient;
+        private readonly IRoatpApiClient _roatpApiClient;
+        private readonly ILogger<GatewayRegisterChecksOrchestrator> _logger;
+
+        public GatewayRegisterChecksOrchestrator(IRoatpApplicationApiClient applyApiClient, IRoatpApiClient roatpApiClient,
+                                                     ILogger<GatewayRegisterChecksOrchestrator> logger)
+        {
+            _applyApiClient = applyApiClient;
+            _roatpApiClient = roatpApiClient;
+            _logger = logger;
+        }
+
+        public async Task<RoatpPageViewModel> GetRoatpViewModel(GetRoatpRequest request)
+        {
+            _logger.LogInformation($"Retrieving RoATP details for application {request.ApplicationId}");
+
+            var pageId = GatewayPageIds.Roatp;
+
+            var commonDetails = await _applyApiClient.GetPageCommonDetails(request.ApplicationId, pageId, request.UserName);
+
+            var model = new RoatpPageViewModel
+            {
+                ApplicationId = request.ApplicationId,
+                PageId = pageId,
+                ApplyLegalName = commonDetails.LegalName,
+                Ukprn = commonDetails.Ukprn,
+                Status = commonDetails.Status,
+                OptionPassText = commonDetails.OptionPassText,
+                OptionFailText = commonDetails.OptionFailText,
+                OptionInProgressText = commonDetails.OptionInProgressText,
+                SourcesCheckedOn = commonDetails.CheckedOn,
+                ApplicationSubmittedOn = commonDetails.ApplicationSubmittedOn,
+                GatewayReviewStatus = commonDetails.GatewayReviewStatus
+            };
+
+            model.ApplyProviderRoute = "TEST - THIS AN END POINT"; // TODO: SORT OUT!!!
+
+            var roatpProviderDetails = await _roatpApiClient.GetOrganisationRegisterStatus(commonDetails.Ukprn);
+
+            if(roatpProviderDetails != null)
+            {
+                model.RoatpUkprnOnRegister = roatpProviderDetails.UkprnOnRegister;
+                model.RoatpStatusDate = roatpProviderDetails.StatusDate;
+                model.RoatpProviderRoute = await GetProviderRoute(roatpProviderDetails.ProviderTypeId);
+                model.RoatpStatus = await GetProviderStatus(roatpProviderDetails.StatusId, roatpProviderDetails.ProviderTypeId);
+                model.RoatpRemoveReason = await GetRemovedReason(roatpProviderDetails.RemovedReasonId);
+            }
+
+            return model;
+        }
+
+        private async Task<string> GetProviderRoute(int? providerTypeId)
+        {
+            string route = null;
+
+            if (providerTypeId.HasValue)
+            {
+                var roatpTypes = await _roatpApiClient.GetProviderTypes();
+
+                route = roatpTypes?.FirstOrDefault(t => t.Id == providerTypeId.Value)?.Type;
+            }
+
+            return route;
+        }
+
+        private async Task<string> GetProviderStatus(int? providerStatusId, int? providerTypeId)
+        {
+            string status = null;
+
+            if(providerStatusId.HasValue)
+            {
+                var roatpStatues = await _roatpApiClient.GetOrganisationStatuses(providerTypeId);
+
+                status = roatpStatues?.FirstOrDefault(s => s.Id == providerStatusId.Value)?.Status;
+            }
+
+            return status;
+        }
+
+        private async Task<string> GetRemovedReason(int? providerRemovedReasonId)
+        {
+            string reason = null;
+
+            if (providerRemovedReasonId.HasValue)
+            {
+                var roatpRemovedReasons = await _roatpApiClient.GetRemovedReasons();
+
+                reason = roatpRemovedReasons?.FirstOrDefault(rm => rm.Id == providerRemovedReasonId.Value)?.Reason;
+            }
+
+            return reason;
+        }
+    }
+}
