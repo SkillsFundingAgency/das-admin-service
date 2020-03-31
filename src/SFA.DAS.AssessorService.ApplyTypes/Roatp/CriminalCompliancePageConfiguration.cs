993e8fee423f360c2b558e7e578af4ff5d57059d
Dave Read <david.read@digital.education.gov.uk>
2020-03-31 11:19:37 +0100

    APR-1459 add config for people in control fraud/irregularities check

--- src/SFA.DAS.AssessorService.ApplyTypes/Roatp/CriminalCompliancePageConfiguration.cs
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/CriminalCompliancePageConfiguration.cs
@@ -20,7 +20,8 @@ namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
             { GatewayPageIds.CriminalComplianceOrganisationChecks.Whistleblowing, "Investigated due to whistleblowing issues in the last 3 months check" },
             { GatewayPageIds.CriminalComplianceOrganisationChecks.Insolvency, "Subject to insolvency or winding up proceedings in the last 3 years check" },
             { GatewayPageIds.CriminalComplianceWhosInControlChecks.UnspentCriminalConvictions, "Unspent criminal convictions check" },
-            { GatewayPageIds.CriminalComplianceWhosInControlChecks.FailedToRepayFunds, "Failed to pay back funds in the last 3 years check" }
+            { GatewayPageIds.CriminalComplianceWhosInControlChecks.FailedToRepayFunds, "Failed to pay back funds in the last 3 years check" },
+            { GatewayPageIds.CriminalComplianceWhosInControlChecks.FraudIrregularities, "Investigated for fraud or irregularities in the last 3 years check" }
         };
     }
 }
