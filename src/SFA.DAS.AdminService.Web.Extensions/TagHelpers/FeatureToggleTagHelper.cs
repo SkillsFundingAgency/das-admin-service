5302048f0b43b9db54e06d9ff299579ab3ddf823
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-01 10:06:44 +0100

    APR-1536 - Added FeatureToggle tag helper

---
+++ src/SFA.DAS.AdminService.Web.Extensions/TagHelpers/FeatureToggleTagHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SFA.DAS.AdminService.Settings;
+
+namespace SFA.DAS.AdminService.Web.Extensions.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = FeatureToggleHelperAttributeName)]
+    [HtmlTargetElement("a", Attributes = FeatureToggleHelperAttributeName)]
+    public class FeatureToggleTagHelper : TagHelper
+    {
+        private const string FeatureToggleHelperAttributeName = "sfa-feature-toggle";
+
+        private readonly IFeatureToggles _featureToggles;
+
+        [HtmlAttributeName(FeatureToggleHelperAttributeName)]
+        public string FeatureToggle { get; set; }
+
+        public FeatureToggleTagHelper(IWebConfiguration configuration)
+        {
+            _featureToggles = configuration?.FeatureToggles;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!IsFeatureToggleEnabled(_featureToggles, FeatureToggle))
+            {
+                output.SuppressOutput();
+            }
+        }
+
+        private static bool IsFeatureToggleEnabled(IFeatureToggles _featureToggles, string featureToggle)
+        {
+            var toggleEnabled = false;
+
+            if (_featureToggles != null && string.IsNullOrWhiteSpace(featureToggle))
+            {
+                try
+                {
+                    var property = _featureToggles.GetType().GetProperty(featureToggle);
+                    var propertyValue = property.GetValue(_featureToggles, null);
+
+                    if (propertyValue != null)
+                    {
+                        toggleEnabled = Convert.ToBoolean(propertyValue);
+                    }
+                }
+                catch (SystemException ex) when (ex is InvalidCastException || ex is FormatException)
+                {
+                    toggleEnabled = false;
+                }
+            }
+
+            return toggleEnabled;
+        }
+    }
+}
---
+++ src/SFA.DAS.AdminService.Web/Domain/FeatureToggles.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.AdminService.Web.Domain
+{
+    public static class FeatureToggles
+    {
+        public const string EnableRoatpApply = "EnableRoatpApply";
+    }
+}
