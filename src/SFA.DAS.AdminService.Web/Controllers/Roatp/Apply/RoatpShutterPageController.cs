652d7cf59a1ee76b59281aeea3170fd37aed317f
Dave Read <david.read@digital.education.gov.uk>
2020-04-03 13:13:14 +0100

    APR-1311 add authorise attribute to shutter page controller

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpShutterPageController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpShutterPageController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.AdminService.Web.Controllers.Roatp.Apply
 {
+    [Authorize]
     public class RoatpShutterPageController : Controller
     {
         [Route("ExternalApisUnavailable")]
