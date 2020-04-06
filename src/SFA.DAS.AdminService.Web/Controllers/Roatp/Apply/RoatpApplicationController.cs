decc9b6ba81e43beec5954ee544c94f6831e8a9d
Dave Read <david.read@digital.education.gov.uk>
2020-04-06 11:41:32 +0100

    APR-1274 update role assignments for Assessor controller

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpApplicationController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpApplicationController.cs
@@ -21,7 +21,7 @@ using SFA.DAS.AdminService.Web.Infrastructure.FeatureToggles;
 
 namespace SFA.DAS.AdminService.Web.Controllers.Roatp.Apply
 {
-    [Authorize(Roles = Roles.AssessmentDeliveryTeam + "," + Roles.CertificationTeam)]
+    [Authorize(Roles = Roles.RoatpAssessorTeam + "," + Roles.RoatpApplicationOversightTeam)]
     [FeatureToggle(FeatureToggles.EnableRoatpAssessorReview, "Dashboard", "Index")]
     public class RoatpApplicationController : Controller
     {
