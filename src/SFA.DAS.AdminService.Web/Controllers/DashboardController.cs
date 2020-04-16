d253f463b16f7940e7a24b4cc3c063fba0f2094d
Martin Lush <martin-hornsby@ntlworld.com>
2020-04-16 15:09:59 +0100

    APR-1590 - Gracefully handle exception when EPAO Assessor Service is down

--- src/SFA.DAS.AdminService.Web/Controllers/DashboardController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/DashboardController.cs
@@ -1,24 +1,50 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.AdminService.Web.Infrastructure;
 using SFA.DAS.AdminService.Web.ViewModels;
+using SFA.DAS.AssessorService.ApplyTypes;
 
 namespace SFA.DAS.AdminService.Web.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+        private readonly ILogger<DashboardController> _logger;
         private readonly IApplicationApiClient _applicationApiClient;
 
-        public DashboardController(IApplicationApiClient applicationApiClient)
+        public DashboardController(ILogger<DashboardController> logger, IApplicationApiClient applicationApiClient)
         {
+            _logger = logger;
             _applicationApiClient = applicationApiClient;
         }
 
         public async Task<IActionResult> Index()
         {
-            var applicationReviewStatusCounts = await _applicationApiClient.GetApplicationReviewStatusCounts();
+            ApplicationReviewStatusCounts applicationReviewStatusCounts;
+
+            try
+            {
+                applicationReviewStatusCounts = await _applicationApiClient.GetApplicationReviewStatusCounts();
+            }
+            catch(HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Unable to GetApplicationReviewStatusCounts from EPAO Service");
+                // Absorb the exception at this point but make it obvious that something has gone wrong. We don't want to kill the Staff Dashboard so soon!
+                applicationReviewStatusCounts = new ApplicationReviewStatusCounts
+                {
+                    OrganisationApplicationsNew = short.MinValue,
+                    OrganisationApplicationsInProgress = short.MinValue,
+                    OrganisationApplicationsHasFeedback = short.MinValue,
+                    OrganisationApplicationsApproved = short.MinValue,
+                    StandardApplicationsNew = short.MinValue,
+                    StandardApplicationsInProgress = short.MinValue,
+                    StandardApplicationsHasFeedback = short.MinValue,
+                    StandardApplicationsApproved = short.MinValue,
+                };
+            }
 
             var viewModel = new DashboardViewModel
             {
