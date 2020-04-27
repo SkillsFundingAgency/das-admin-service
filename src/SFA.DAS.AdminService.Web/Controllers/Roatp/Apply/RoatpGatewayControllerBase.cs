c731f06b7369e147782961fa1032ccd32975ff9a
Michael Young <mick.young1981@googlemail.com>
2020-04-27 09:40:30 +0100

    Readded auth attribute

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpGatewayControllerBase.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/Apply/RoatpGatewayControllerBase.cs
@@ -16,6 +16,7 @@ using SFA.DAS.AssessorService.ApplyTypes.Roatp;
 
 namespace SFA.DAS.AdminService.Web.Controllers.Roatp.Apply
 {
+    [Authorize(Roles = Roles.RoatpGatewayAssessorTeam)]
     [FeatureToggle(FeatureToggles.EnableRoatpGatewayReview, "Dashboard", "Index")]
     public class RoatpGatewayControllerBase<T> : Controller
     {
