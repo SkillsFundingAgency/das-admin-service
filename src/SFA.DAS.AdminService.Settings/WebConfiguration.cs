67508d61f9a7dd99caaba850badeb8bee4e965bc
Dave Read <david.read@digital.education.gov.uk>
2020-05-28 14:50:32 +0100

    APR-1753 add RoATP gateway base URL to config, change dashboard link

--- src/SFA.DAS.AdminService.Settings/WebConfiguration.cs
+++ src/SFA.DAS.AdminService.Settings/WebConfiguration.cs
@@ -24,6 +24,7 @@ namespace SFA.DAS.AdminService.Settings
         [JsonRequired] public string RoatpApiClientBaseUrl { get; set; }
 
         [JsonRequired] public string RoatpOversightBaseUrl { get; set; }
+        [JsonRequired] public string RoatpGatewayBaseUrl { get; set; }
         [JsonRequired] public ClientApiAuthentication RoatpApiAuthentication { get; set; }
         [JsonRequired] public ClientApiAuthentication QnaApiAuthentication { get; set; }
 
