18cb7b7b6be8f7db18450ade31011535aa99d3f3
Shoma Gujjar <Shoma.Gujjar@valtech.co.uk>
2020-06-08 13:03:43 +0100

    CON-1825-fix word count

---
+++ src/SFA.DAS.AdminService.Web/Validators/CertificateDeleteViewModelValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using System;
+using SFA.DAS.AdminService.Web.ViewModels.CertificateDelete;
+
+namespace SFA.DAS.AdminService.Web.Validators.Roatp.Applications
+{
+    public class CertificateDeleteViewModelValidator : AbstractValidator<CertificateAuditDetailsViewModel>
+    {
+        public CertificateDeleteViewModelValidator()
+        {
+            RuleFor(vm => vm).Custom((vm, context) =>
+            {
+                if (HasExceededWordCount(vm.ReasonForChange))
+                {
+                    context.AddFailure("ReasonForChange", "Reason for deleting this certificate must be 500 words or less");
+                }
+            });
+        }
+
+        private static bool HasExceededWordCount(string input, int maxWordcount = 500)
+        {
+            bool hasExceeded = false;
+
+            var text = input?.Trim();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                var wordCount = text.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+                hasExceeded = (wordCount > maxWordcount);
+            }
+
+            return hasExceeded;
+        }
+    }
+}
