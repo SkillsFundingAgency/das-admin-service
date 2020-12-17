542f858e5333ff2ebdc8a8175bbac4ddc3325952
gdbarnes <gregdbarnes@gmail.com>
2020-12-17 15:37:17 +0000

    Add application download view, markup and datepicker assets

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/RoatpHomeController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/RoatpHomeController.cs
@@ -1,16 +1,22 @@
 ﻿namespace SFA.DAS.AdminService.Web.Controllers.Roatp
 {
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using SFA.DAS.AdminService.Web.Domain;
+  using Microsoft.AspNetCore.Authorization;
+  using Microsoft.AspNetCore.Mvc;
+  using SFA.DAS.AdminService.Web.Domain;
 
-    [Authorize(Roles = Roles.RoatpGatewayTeam)]
-    public class RoatpHomeController : Controller
+  [Authorize(Roles = Roles.RoatpGatewayTeam)]
+  public class RoatpHomeController : Controller
+  {
+    [Route("search-apprenticeship-training-providers")]
+    public IActionResult Index()
     {
-        [Route("search-apprenticeship-training-providers")]
-        public IActionResult Index()
-        {           
-            return View("~/Views/Roatp/Index.cshtml");
-        }
+      return View("~/Views/Roatp/Index.cshtml");
     }
+
+    [Route("application-download")]
+    public IActionResult ApplicationDownload()
+    {
+      return View("~/Views/Roatp/ApplicationDownload.cshtml");
+    }
+  }
 }
---
+++ src/SFA.DAS.AdminService.Web/wwwroot/javascripts/das-datepicker.js
@@ -0,0 +1,49 @@
+(function () {
+  var flatpickrOptions = {
+    allowInput: true,
+    maxDate: "today",
+    wrap: true,
+    disableMobile: "true",
+    // altInput: true,
+    // altFormat: "j F Y", // GDS date format: 30 June 2020
+  };
+
+  var fromFlatpickr = flatpickr(".js-flatpickr-from", flatpickrOptions);
+  var toFlatpickr = flatpickr(".js-flatpickr-to", flatpickrOptions);
+
+  document
+    .querySelector(".js-flatpickr-from-input")
+    .addEventListener("blur", function () {
+      fromFlatpickr.setDate(this.value, true);
+    });
+
+  document
+    .querySelector(".js-flatpickr-to-input")
+    .addEventListener("blur", function () {
+      toFlatpickr.setDate(this.value, true);
+    });
+
+  var removeSelectedClassFromNodeList = function (nodeListSelector) {
+    var nodeList = document.querySelectorAll("." + nodeListSelector);
+    for (var i = 0; i < nodeList.length; i++) {
+      nodeList[i].classList.remove(nodeListSelector);
+    }
+  };
+
+  document
+    .querySelector('[data-module="das-clear-form-errors"]')
+    .addEventListener("submit", function () {
+      var errorSummary = document.querySelector(".govuk-error-summary");
+      var errorMessages = document.querySelectorAll(".govuk-error-message");
+
+      if (!errorSummary || !errorMessages) return;
+
+      errorSummary.style.display = "none";
+      for (var i = 0; i < errorMessages.length; i++) {
+        errorMessages[i].style.display = "none";
+      }
+      removeSelectedClassFromNodeList("govuk-form-group--error");
+      removeSelectedClassFromNodeList("govuk-select--error");
+      removeSelectedClassFromNodeList("govuk-input--error");
+    });
+})();
