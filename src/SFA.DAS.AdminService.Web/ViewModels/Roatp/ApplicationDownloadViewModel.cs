747efda7c53236337f20fb3810b806cd443600ca
chrisfoster186 <chrisfoster186@googlemail.com>
2020-12-22 14:31:11 +0000

    Suppresses default model binder failure errors for date times in report download vm

--- src/SFA.DAS.AdminService.Web/ViewModels/Roatp/ApplicationDownloadViewModel.cs
+++ src/SFA.DAS.AdminService.Web/ViewModels/Roatp/ApplicationDownloadViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using SFA.DAS.AdminService.Web.Attributes;
 
 namespace SFA.DAS.AdminService.Web.ViewModels.Roatp
 {
     public class ApplicationDownloadViewModel
     {
+        [SuppressBindingErrors]
         public DateTime? FromDate { get; set; }
+        [SuppressBindingErrors]
         public DateTime? ToDate { get; set; }
     }
 }
