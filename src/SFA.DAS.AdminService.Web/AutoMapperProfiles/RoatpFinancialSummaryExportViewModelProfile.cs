93e330d8584d3df5395ddebaa149ed590c1fdda9
Martin Lush <martin-hornsby@ntlworld.com>
2021-01-20 14:41:15 +0000

    APR-2149 - Updated AutoMapper profile due to Apply changes

--- src/SFA.DAS.AdminService.Web/AutoMapperProfiles/RoatpFinancialSummaryExportViewModelProfile.cs
+++ src/SFA.DAS.AdminService.Web/AutoMapperProfiles/RoatpFinancialSummaryExportViewModelProfile.cs
@@ -15,7 +15,7 @@ namespace SFA.DAS.AdminService.Web.AutoMapperProfiles
                 .ForMember(dest => dest.ProviderName, opt => opt.MapFrom(source => source.OrganisationName))
                 .ForMember(dest => dest.Ukprn, opt => opt.MapFrom(source => source.Ukprn))
                 .ForMember(dest => dest.SubmissionDate, opt => opt.MapFrom(source => source.SubmittedDate))
-                .ForMember(dest => dest.GatewayCompletionDate, opt => opt.MapFrom(source => source.GatewayOutcomeDateTime))
+                .ForMember(dest => dest.GatewayCompletionDate, opt => opt.MapFrom(source => source.GatewayOutcomeDate))
                 .ForMember(dest => dest.CharityNo, opt => opt.MapFrom(source => source.CharityNumber))
                 .ForMember(dest => dest.CompanyNo, opt => opt.MapFrom(source => source.CompanyNumber))
                 .ForMember(dest => dest.TurnOver, opt => opt.MapFrom(source => source.FinancialData.TurnOver))
