fc0584eb499dc4256e1786ea477c0fb776f4888b
Matt Daniels <matt@techtoniq.com>
2021-06-02 11:00:07 +0100

    Display version

--- src/SFA.DAS.AssessorService.Api.Types/Models/Staff/LearnerDetailResult.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/Staff/LearnerDetailResult.cs
@@ -12,6 +12,7 @@ namespace SFA.DAS.AssessorService.Api.Types.Models.Staff
         public int StandardCode { get; set; }
         public string Standard { get; set; }
         public int Level { get; set; }
+        public string Version { get; set; }
         public int? FundingModel { get; set; }
         public int? CompletionStatus { get; set; }
         public string CompletionStatusDescription { get; set; }
