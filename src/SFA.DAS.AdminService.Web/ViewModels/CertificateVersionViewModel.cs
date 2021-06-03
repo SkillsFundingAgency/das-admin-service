e7a8978a2a7ed9fef595f39b88a023f8efb4f653
Matt Daniels <matt@techtoniq.com>
2021-06-03 16:18:04 +0100

    Relocate the collection to the more specific view.

--- src/SFA.DAS.AdminService.Web/ViewModels/CertificateVersionViewModel.cs
+++ src/SFA.DAS.AdminService.Web/ViewModels/CertificateVersionViewModel.cs
@@ -1,14 +1,15 @@
 ﻿using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Api.Types.Models.Standards;
 using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Domain.JsonData;
+using System.Collections.Generic;
 
 namespace SFA.DAS.AdminService.Web.ViewModels
 {
     public class CertificateVersionViewModel : CertificateBaseViewModel, ICertificateViewModel
     {
+        public List<StandardVersion> Standards { get; set; }
         public string SelectedVersion { get; set; }
-
-
         public int StandardCode { get; set; }
 
         public void FromCertificate(Certificate cert)
