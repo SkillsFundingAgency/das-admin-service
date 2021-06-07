ae3dc692a7e452e624e867f3c1333aaf81f5e5b7
Matt Daniels <matt@techtoniq.com>
2021-06-07 10:55:37 +0100

    Functionality to change Standard Option implemented as radio buttons instead of free text.

--- src/SFA.DAS.AdminService.Web/ViewModels/CertificateOptionViewModel.cs
+++ src/SFA.DAS.AdminService.Web/ViewModels/CertificateOptionViewModel.cs
@@ -1,6 +1,9 @@
 using Newtonsoft.Json;
+using SFA.DAS.AdminService.Web.Infrastructure;
 using SFA.DAS.AssessorService.Domain.Entities;
 using SFA.DAS.AssessorService.Domain.JsonData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.AdminService.Web.ViewModels
 {
@@ -8,16 +11,20 @@ namespace SFA.DAS.AdminService.Web.ViewModels
     {
         public bool HasAdditionalLearningOption { get; set; }
         public string Option { get; set; }
+        public string SelectedOption { get; set; }
+        public List<Option> Options { get; set; }
+        public int StandardCode { get; set; }
         public void FromCertificate(Certificate cert)
         {
             BaseFromCertificate(cert);
             Option = CertificateData.CourseOption;
             HasAdditionalLearningOption = !string.IsNullOrWhiteSpace(Option);
+            StandardCode = cert.StandardCode;
         }
 
         public Certificate GetCertificateFromViewModel(Certificate certificate, CertificateData data)
         {
-            data.CourseOption = HasAdditionalLearningOption ? Option : string.Empty;
+            data.CourseOption = SelectedOption;
             certificate.CertificateData = JsonConvert.SerializeObject(data);
 
             return certificate;
