319b1f095eb54342e77befa0691c451f4f4daddf
Ben Stone <ben1.stone@education.gov.uk>
2021-07-01 11:21:18 +0100

    SV-929 Add view and controller outline for edit standard version

---
+++ src/SFA.DAS.AdminService.Web/Validators/RegisterEditOrganisationStandardVersionViewModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using SFA.DAS.AdminService.Web.ViewModels.Register;
+
+namespace SFA.DAS.AdminService.Web.Validators
+{
+    public class RegisterEditOrganisationStandardVersionViewModelValidator : AbstractValidator<RegisterEditOrganisationStandardVersionViewModel>
+    {
+        public RegisterEditOrganisationStandardVersionViewModelValidator()
+        {
+        }
+    }
+}
