ebfa62b7ce7e3784a8e552da588b47b62bd6f7b0
Ben Stone <ben1.stone@education.gov.uk>
2021-07-06 15:12:55 +0100

    SV-930 Improve validation

--- src/SFA.DAS.AdminService.Web/Validators/RegisterEditOrganisationStandardVersionViewModelValidator.cs
+++ src/SFA.DAS.AdminService.Web/Validators/RegisterEditOrganisationStandardVersionViewModelValidator.cs
@@ -1,29 +1,50 @@
 ﻿using FluentValidation;
+using FluentValidation.Validators;
+using SFA.DAS.AdminService.Common.Validation;
 using SFA.DAS.AdminService.Web.Extensions;
+using SFA.DAS.AdminService.Web.Helpers;
 using SFA.DAS.AdminService.Web.ViewModels.Register;
-using System;
+using System.Collections.Generic;
 
 namespace SFA.DAS.AdminService.Web.Validators
 {
     public class RegisterEditOrganisationStandardVersionViewModelValidator : AbstractValidator<RegisterEditOrganisationStandardVersionViewModel>
     {
-        public RegisterEditOrganisationStandardVersionViewModelValidator()
+        private readonly IRegisterValidator _registerValidator;
+
+        public RegisterEditOrganisationStandardVersionViewModelValidator(IRegisterValidator registerValidator)
         {
+            _registerValidator = registerValidator;
+            var errorInEffectiveFrom = false;
 
             RuleFor(vm => vm).Custom((vm, context) =>
             {
+                var validationResultEffectiveFrom = registerValidator.CheckDateIsEmptyOrValid(vm.EffectiveFromDay,
+                    vm.EffectiveFromMonth,
+                    vm.EffectiveFromYear, "EffectiveFromDay",
+                    "EffectiveFromMonth", "EffectiveFromYear", "EffectiveFrom", "Effective From");
+
+                errorInEffectiveFrom = validationResultEffectiveFrom.Errors.Count > 0;
+
+                var validationResultEffectiveTo = _registerValidator.CheckDateIsEmptyOrValid(vm.EffectiveToDay,
+                    vm.EffectiveToMonth,
+                    vm.EffectiveToYear, "EffectiveToDay",
+                    "EffectiveToMonth", "EffectiveToYear", "EffectiveTo", "Effective To");
+
                 vm.EffectiveFrom = ValidatorExtensions.ConstructDate(vm.EffectiveFromDay, vm.EffectiveFromMonth, vm.EffectiveFromYear);
                 vm.EffectiveTo = ValidatorExtensions.ConstructDate(vm.EffectiveToDay, vm.EffectiveToMonth, vm.EffectiveToYear);
 
-                if (vm.EffectiveTo is null)
-                {
-                    context.AddFailure("EffectiveTo", "Enter a valid date");
-                }
-                else if (vm.EffectiveTo.Value < DateTime.UtcNow)
-                {
-                    context.AddFailure("EffectiveTo", "The date must be in the future");
-                }
+                CreateFailuresInContext(validationResultEffectiveFrom.Errors, context);
+                CreateFailuresInContext(validationResultEffectiveTo.Errors, context);
             });
+        }
+
+        private static void CreateFailuresInContext(IEnumerable<ValidationErrorDetail> errs, CustomContext context)
+        {
+            foreach (var error in errs)
+            {
+                context.AddFailure(error.Field, error.ErrorMessage);
+            }
         }
     }
 }
