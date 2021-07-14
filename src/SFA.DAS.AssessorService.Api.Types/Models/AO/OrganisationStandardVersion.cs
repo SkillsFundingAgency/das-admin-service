b1418d104caff5a3deb9d928243da9ecda00be8c
ben1stone <54842691+ben1stone@users.noreply.github.com>
2021-07-14 09:13:48 +0100

    SV-930 edit standard version page (#265)
    * SV-929 Add view and controller outline for edit standard version
    
    * SV-929 Add api call
    
    * SV-930 Submit edit page with validator for EffectiveTo
    
    * SV-930 Improve validation
    
    * SV-930 Call UpdateStandardVersionValidator api
    
    * SV-930 Fix back link on Edit standard page
    
    * SV-930 update view
    
    * SV-927 include versions on edit epao details (#266)
    
    * SV-927 Include versions on edit epao details
    
    * SV-927 Edit organisation view
    
    * SV-983 Add orgStandardId to update org standard request (#267)

--- src/SFA.DAS.AssessorService.Api.Types/Models/AO/OrganisationStandardVersion.cs
+++ src/SFA.DAS.AssessorService.Api.Types/Models/AO/OrganisationStandardVersion.cs
@@ -8,5 +8,6 @@ namespace SFA.DAS.AssessorService.Api.Types.Models.AO
         public DateTime? EffectiveTo { get; set; }
         public DateTime? DateVersionApproved { get; set; }
         public string Version { get; set; }
+        public string Status { get; set; }
     }
 }
