06db33ca319810ee7f23d5e44f88771676acab8f
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-02-10 10:43:19 +0000

    APR-2953- removed added to string constant

--- src/SFA.DAS.AdminService.Web/AutoMapperProfiles/RoatpOversightOutcomeExportViewModelProfile.cs
+++ src/SFA.DAS.AdminService.Web/AutoMapperProfiles/RoatpOversightOutcomeExportViewModelProfile.cs
@@ -14,7 +14,7 @@ namespace SFA.DAS.AdminService.Web.AutoMapperProfiles
                 .ForMember(dest => dest.ApplicationReferenceNumber, opt => opt.MapFrom(source => source.ApplicationReferenceNumber))
                 .ForMember(dest => dest.ApplicationSubmittedDate, opt => opt.MapFrom(source => source.ApplicationSubmittedDate))
                 .ForMember(dest => dest.OrganisationName, opt => opt.MapFrom(source => source.OrganisationName))
-                .ForMember(dest => dest.IsOnRegister, opt => opt.MapFrom(source => !string.IsNullOrWhiteSpace(source.ProviderRouteNameOnRegister) && source.OrganisationStatus != "Removed"))
+                .ForMember(dest => dest.IsOnRegister, opt => opt.MapFrom(source => !string.IsNullOrWhiteSpace(source.ProviderRouteNameOnRegister) && source.OrganisationStatus != OrganisationStatus.Removed))
                 .ForMember(dest => dest.ProviderRouteNameOnRegister, opt => opt.MapFrom(source => source.ProviderRouteNameOnRegister))
                 .ForMember(dest => dest.OrganisationType, opt => opt.MapFrom(source => source.OrganisationType))
                 .ForMember(dest => dest.CompanyNumber, opt => opt.MapFrom(source => source.CompanyNumber));
---
+++ src/SFA.DAS.AssessorService.ApplyTypes/Roatp/OrganisationStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.AssessorService.ApplyTypes.Roatp
+{
+    public static class OrganisationStatus
+    {
+        public const string Removed = "Removed";
+    }
+}
