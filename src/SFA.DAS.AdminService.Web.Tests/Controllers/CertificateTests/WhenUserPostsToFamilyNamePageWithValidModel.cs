c6c6e748e0499f90661be769ac3a06ffdf80ae82
CodeWolf86 <54770494+CodeWolf86@users.noreply.github.com>
2022-02-28 11:47:41 +0000

    SV-1387 editing of apprentice names for certificate (#335)
    * Re-Add editing of names for certifcate
    
    * Rework after rebase and fix issues
    
    * Rework Unit Tests
    
    * Sonar Cloud Update
    
    Co-authored-by: umakanth-g <45845799+umakanth-g@users.noreply.github.com>

---
+++ src/SFA.DAS.AdminService.Web.Tests/Controllers/CertificateTests/WhenUserPostsToFamilyNamePageWithValidModel.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SFA.DAS.AdminService.Web.Controllers;
+using SFA.DAS.AdminService.Web.ViewModels.CertificateAmend;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AdminService.Web.Tests.Controllers.CertificateTests
+{
+    public class WhenUserPostsToFamilyNamePageWithValidModel : CertificateAmendQueryBase
+    {
+        private IActionResult _result;
+
+        [SetUp]
+        public void Arrange()
+        {
+            var controller = new CertificateNamesController(MockedLogger.Object, MockHttpContextAccessor.Object, ApiClient);
+            var validModel = new CertificateFamilyNameViewModel { Id = Certificate.Id, FamilyName = "Testing", ReasonForChange = "Change Name" };
+            _result = controller.FamilyName(validModel).GetAwaiter().GetResult();
+        }
+
+        [Test]
+        public void ThenShouldReturnCheckView()
+        {
+            var result = _result as RedirectToActionResult;
+            result.ActionName.Should().Be("Check");
+            result.ControllerName.Should().Be("CertificateAmend");
+        }
+    }
+}
---
+++ src/SFA.DAS.AdminService.Web/ViewModels/CertificateAmend/CertificateFamilyNameViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SFA.DAS.AssessorService.Domain.Entities;
+using SFA.DAS.AssessorService.Domain.JsonData;
+
+namespace SFA.DAS.AdminService.Web.ViewModels.CertificateAmend
+{
+    public class CertificateFamilyNameViewModel : CertificateBaseViewModel, ICertificateViewModel
+    {
+        public override void FromCertificate(Certificate cert)
+        {
+            base.FromCertificate(cert);
+            FamilyName = CertificateData.LearnerFamilyName;
+            FullName = CertificateData.FullName;
+        }
+
+        public override Certificate GetCertificateFromViewModel(Certificate certificate, CertificateData certData)
+        {
+            certData.LearnerFamilyName = FamilyName;
+            certData.FullName = $"{certData.LearnerGivenNames} {certData.LearnerFamilyName}";
+
+            certificate.CertificateData = JsonConvert.SerializeObject(certData);
+            return certificate;
+        }
+    }
+}
