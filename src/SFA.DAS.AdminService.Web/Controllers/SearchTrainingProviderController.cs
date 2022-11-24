b6a8cb6771c86bf1414c8f1d352d97f66ba26697
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-11-24 16:43:40 +0000

    CSP-379 Tribal role accessing manage provider description

---
+++ src/SFA.DAS.AdminService.Web/Controllers/SearchTrainingProviderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AdminService.Web.Controllers
+{
+    [Authorize(Roles = Domain.Roles.TribalTeam)]
+    public class SearchTrainingProviderController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
