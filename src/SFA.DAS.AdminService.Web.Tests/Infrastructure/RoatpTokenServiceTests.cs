5ab8beb4c1d0c400317702f3b7f945849bb79c8b
Saw Naw <saw.naw@digital.education.gov.uk>
2023-02-01 14:22:06 +0000

    Slightly simplify test

--- src/SFA.DAS.AdminService.Web.Tests/Infrastructure/RoatpTokenServiceTests.cs
+++ src/SFA.DAS.AdminService.Web.Tests/Infrastructure/RoatpTokenServiceTests.cs
@@ -10,31 +10,29 @@ namespace SFA.DAS.AdminService.Web.Tests.Infrastructure
 {
     public class RoatpTokenServiceTests
     {
+        private const string roAtpApiV1Resource = @"https://citizenazuresfabisgov.onmicrosoft.com/das-at-roatpapi-as-ar";
+
         [Test]
         public void GetToken_Returns_Valid_Jwt_Token()
         {
-            const string resource = @"https://citizenazuresfabisgov.onmicrosoft.com/das-at-roatpapi-as-ar";
-            var sut = CreateSut(resource);
+            var sut = CreateSut();
 
             var token = sut.GetToken();
-            var tokenHandler = new JwtSecurityTokenHandler();
 
+            var tokenHandler = new JwtSecurityTokenHandler();
             Assert.That(tokenHandler.CanReadToken(token), Is.True);
         }
 
-        private static RoatpTokenService CreateSut(string resource)
+        private static RoatpTokenService CreateSut()
         {
             var webConfiguration = new Mock<IWebConfiguration>();
-
             var authentication = new ClientApiAuthentication
             {
-                ResourceId = resource
+                ResourceId = roAtpApiV1Resource
             };
-
             webConfiguration.SetupGet(x => x.RoatpApiAuthentication).Returns(authentication);
-            var hostEnvironment = new Mock<IHostEnvironment>();
 
-            return new RoatpTokenService(webConfiguration.Object, hostEnvironment.Object);
+            return new RoatpTokenService(webConfiguration.Object, new Mock<IHostEnvironment>().Object);
         }
     }
 }
