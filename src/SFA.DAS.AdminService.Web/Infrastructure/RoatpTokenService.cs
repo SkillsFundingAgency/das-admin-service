52f955491ce44b14a676e277eba3eae6f8c91ee6
Chris Woodcock <chris@humesoftware.com>
2023-03-23 14:23:57 +0000

    Backed out more code to see if it fixes deployment issue

--- src/SFA.DAS.AdminService.Web/Infrastructure/RoatpTokenService.cs
+++ src/SFA.DAS.AdminService.Web/Infrastructure/RoatpTokenService.cs
@@ -1,29 +1,35 @@
 ﻿using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using SFA.DAS.AdminService.Settings;
-//using Azure.Identity;
-//using Azure.Core;
 
 namespace SFA.DAS.AdminService.Web.Infrastructure
 {
     public class RoatpTokenService : IRoatpTokenService
     {
         private readonly IWebConfiguration _configuration;
-        private readonly IHostEnvironment _hostEnvironment;
-        public RoatpTokenService(IWebConfiguration configuration, IHostEnvironment hostEnvironment)
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public RoatpTokenService(IWebConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             _configuration = configuration;
-            _hostEnvironment = hostEnvironment;
+            _hostingEnvironment = hostingEnvironment;
         }
+
         public string GetToken()
         {
-            if (_hostEnvironment.IsDevelopment())
+            if (_hostingEnvironment.IsDevelopment())
                 return string.Empty;
 
-            //var result = new DefaultAzureCredential().GetTokenAsync(
-            //    new TokenRequestContext(scopes: new string[] { _configuration.RoatpApiAuthentication.ResourceId + "/.default" }) { }).Result;
+            var tenantId = _configuration.RoatpApiAuthentication.TenantId;
+            var clientId = _configuration.RoatpApiAuthentication.ClientId;
+            var appKey = _configuration.RoatpApiAuthentication.ClientSecret;
+            var resourceId = _configuration.RoatpApiAuthentication.ResourceId;
 
-            //return result.Token;
-            return string.Empty;
+            var authority = $"https://login.microsoftonline.com/{tenantId}";
+            var clientCredential = new ClientCredential(clientId, appKey);
+            var context = new AuthenticationContext(authority, true);
+            var result = context.AcquireTokenAsync(resourceId, clientCredential).Result;
+
+            return result.AccessToken;
         }
     }
 }
