1dd9ac99468b9ce75097a8ae80887e8d51403d2e
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-10-13 13:23:09 +0100

    Commit Details:
    1. Amended Access Denied Page for DfESignIn Users.
    2. Unit Testing included with the test coverage for newly added code.
    3. DfESignIn Auth PKG upgraded to the Latest v17.1.56
    
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-940

--- src/SFA.DAS.AdminService.Web.UnitTests/Controllers/Home/HomeControllerTest.cs
+++ src/SFA.DAS.AdminService.Web.UnitTests/Controllers/Home/HomeControllerTest.cs
@@ -7,6 +7,7 @@ using SFA.DAS.AdminService.Settings;
 using SFA.DAS.AdminService.Web.Controllers;
 using SFA.DAS.AdminService.Web.Models;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
 
 namespace SFA.DAS.AdminService.Web.UnitTests.Controllers.Home
 {
@@ -14,12 +15,14 @@ namespace SFA.DAS.AdminService.Web.UnitTests.Controllers.Home
     public class HomeControllerTest
     {
         private HomeController _controller;
-        private Mock<IWebConfiguration> _configuration;
+        private Mock<IWebConfiguration> _mockWebConfiguration;
+        private Mock<IConfiguration> _mockConfiguration;
 
         [SetUp]
         public void Setup()
         {
-            _configuration = new Mock<IWebConfiguration>();
+            _mockWebConfiguration = new Mock<IWebConfiguration>();
+            _mockConfiguration = new Mock<IConfiguration>();
         }
 
 
@@ -28,10 +31,10 @@ namespace SFA.DAS.AdminService.Web.UnitTests.Controllers.Home
         public void Then_Index_Returns_ViewModel(bool useDfESignIn)
         {
             //arrange
-            _configuration.Setup(c => c.UseDfESignIn).Returns(useDfESignIn);
-            _controller = new HomeController(_configuration.Object)
+            _mockWebConfiguration.Setup(c => c.UseDfESignIn).Returns(useDfESignIn);
+            _controller = new HomeController(_mockWebConfiguration.Object, _mockConfiguration.Object)
             {
-                ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext() },
+                ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext() },
             };
 
             //sut
@@ -49,26 +52,48 @@ namespace SFA.DAS.AdminService.Web.UnitTests.Controllers.Home
         public void When_DfESignIn_True_And_User_Authenticated_Then_Index_Returns_Redirect()
         {
             //arrange
-            _configuration.Setup(c => c.UseDfESignIn).Returns(true);
+            _mockWebConfiguration.Setup(c => c.UseDfESignIn).Returns(true);
             var authorizedUser = new ClaimsPrincipal(new ClaimsIdentity(new[]
             {
                 new Claim("name", "somename")
             }, "mock"));
             var httpContext = new Mock<HttpContext>();
             httpContext.Setup(c => c.User).Returns(authorizedUser);
-            var controller = new HomeController(_configuration.Object)
+            _controller = new HomeController(_mockWebConfiguration.Object, _mockConfiguration.Object)
             {
                 ControllerContext = new ControllerContext { HttpContext = httpContext.Object }
             };
 
             //sut
-            var result = controller.Index();
+            var result = _controller.Index();
 
             //assert
             result.Should().NotBeNull();
             var actualResult = (RedirectToActionResult)result;
             actualResult.ControllerName.Should().Be("Dashboard");
             actualResult.ActionName.Should().Be("Index");
+        }
+
+        [TestCase("test", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", true)]
+        [TestCase("pp", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", true)]
+        [TestCase("local", "https://test-services.signin.education.gov.uk/approvals/select-organisation?action=request-service", false)]
+        [TestCase("prd", "https://services.signin.education.gov.uk/approvals/select-organisation?action=request-service", false)]
+        public void When_InvalidRole_Then_ViewIsReturned(string env, string helpLink, bool useDfESignIn)
+        {
+            //arrange
+            _mockWebConfiguration.Setup(x => x.UseDfESignIn).Returns(useDfESignIn);
+            _mockConfiguration.Setup(x => x["ResourceEnvironmentName"]).Returns(env);
+            _controller = new HomeController(_mockWebConfiguration.Object, _mockConfiguration.Object);
+
+            //sut
+            var result = (ViewResult)_controller.InvalidRole();
+
+            //assert
+            Assert.That(result, Is.Not.Null);
+
+            var actualModel = result.Model as Error403ViewModel;
+            Assert.That(actualModel?.HelpPageLink, Is.EqualTo(helpLink));
+            Assert.That(actualModel?.UseDfESignIn, Is.EqualTo(useDfESignIn));
         }
     }
 }
--- src/SFA.DAS.AdminService.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SFA.DAS.AdminService.Settings;
 using SFA.DAS.AdminService.Web.Models;
 
@@ -6,19 +7,23 @@ namespace SFA.DAS.AdminService.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IWebConfiguration _configuration;
+        private readonly IWebConfiguration _webConfiguration;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(IWebConfiguration configuration)
+        public HomeController(
+            IWebConfiguration webConfiguration,
+            IConfiguration configuration)
         {
+            _webConfiguration = webConfiguration;
             _configuration = configuration;
         }
 
         public IActionResult Index()
         {
             // if the user is already signed in, then redirect the user to the Dashboard index page.
-            if (_configuration.UseDfESignIn && User.Identity != null && User.Identity.IsAuthenticated) return RedirectToAction("Index", "Dashboard");
+            if (_webConfiguration.UseDfESignIn && User.Identity != null && User.Identity.IsAuthenticated) return RedirectToAction("Index", "Dashboard");
 
-            return View(new HomeViewModel { UseDfESignIn = _configuration.UseDfESignIn });
+            return View(new HomeViewModel { UseDfESignIn = _webConfiguration.UseDfESignIn });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -29,7 +34,7 @@ namespace SFA.DAS.AdminService.Web.Controllers
 
         public IActionResult InvalidRole()
         {
-            return View();
+            return View(new Error403ViewModel(_configuration["ResourceEnvironmentName"]) { UseDfESignIn = _webConfiguration.UseDfESignIn });
         }
     }
 }
