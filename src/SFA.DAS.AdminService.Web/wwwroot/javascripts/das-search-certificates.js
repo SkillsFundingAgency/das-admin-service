6ff779cdc05cd1316789de0c5e419e14a604118d
Karen Hanna <karen@laveryhanna.com>
2025-02-13 12:37:55 +0000

    search input screen

---
+++ src/SFA.DAS.AdminService.Web/wwwroot/javascripts/das-search-certificates.js
@@ -0,0 +1,20 @@
+﻿(function ($) {
+
+    $(document).ready(function () {
+
+        $("#standardButton").click(function (event) {
+            $("#searchMode").val(window.searchmodes.standards);
+            $("#FirstName").val("");
+            $("#LastName").val("");
+            $("#Day").val("");
+            $("#Month").val("");
+            $("#Year").val("");
+        });
+
+        $("#frameworkButton").click(function (event) {
+            $("#searchMode").val(window.searchmodes.frameworks);
+            $("#SearchString").val("");
+        });
+    });
+
+})(jQuery);
