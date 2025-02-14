30e7345db4996062045dc651d99de65e51d7ea47
Karen Hanna <karen@laveryhanna.com>
2025-02-14 13:30:58 +0000

    pr comments

--- src/SFA.DAS.AdminService.Web/Validators/SearchViewModelValidator.cs
+++ src/SFA.DAS.AdminService.Web/Validators/SearchViewModelValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SFA.DAS.AdminService.Web.Controllers;
+using SFA.DAS.AdminService.Web.Extensions;
 using SFA.DAS.AssessorService.Web.Infrastructure;
 using System;
 using System.Security.Cryptography;
@@ -10,94 +11,59 @@ namespace SFA.DAS.AdminService.Web.Validators
     {
         public SearchInputViewModelValidator()
         {
-            When(vm => vm.SearchMode == SearchMode.Standards, () =>
+            When(vm => vm.SearchType == SearchTypes.Standards, () =>
             {
                 RuleFor(vm => vm.SearchString)
+                    .Cascade(CascadeMode.Stop)
                     .NotEmpty()
                     .WithMessage("Enter name or number")
                     .Must(x => x == null || x.Trim().Length > 1)
                     .WithMessage("Name or number must be at least 2 characters.");
             });
 
-            When(vm => vm.SearchMode == SearchMode.Frameworks, () =>
+            When(vm => vm.SearchType == SearchTypes.Frameworks, () =>
             {
                 RuleFor(vm => vm.FirstName)
+                    .Cascade(CascadeMode.Stop)
                     .NotEmpty()
                     .WithMessage("Enter first name")
                     .Must(x => x == null || x.Trim().Length > 1)
                     .WithMessage("First name must be at least 2 characters.");
 
                 RuleFor(vm => vm.LastName)
+                    .Cascade(CascadeMode.Stop)
                     .NotEmpty()
                     .WithMessage("Enter last name")
                     .Must(x => x == null || x.Trim().Length > 1)
                     .WithMessage("Last name must be at least 2 characters.");
 
-                RuleFor(vm => vm.Day).NotEmpty().WithMessage("The date of birth must include day").DependentRules(() =>
-                {
-                    RuleFor(vm => vm.Day).Must(BeANumber).WithMessage("Day must be a number").DependentRules(() =>
-                    {
-                        RuleFor(vm => vm.Day).Must(BeInValidDayRange).WithMessage("Enter a valid day");
-                    });
-                });
-
-                RuleFor(vm => vm.Month).NotEmpty().WithMessage("The date of birth must include month").DependentRules(() =>
-                {
-                    RuleFor(vm => vm.Month).Must(BeANumber).WithMessage("Month must be a number").DependentRules(() =>
-                    {
-                        RuleFor(vm => vm.Month).Must(BeInValidMonthRange).WithMessage("Enter a valid month");
-                    });
-                });
-
-                RuleFor(vm => vm.Year).NotEmpty().WithMessage("The date of birth must include year").DependentRules(() =>
-                {
-                    RuleFor(vm => vm.Year).Length(4).WithMessage("Enter a valid year").DependentRules(() =>
-                    {
-                        RuleFor(vm => vm.Year).Must(BeANumber).WithMessage("Year must be a number");
-                    });
-                });
-
                 RuleFor(vm => vm).Custom((vm, context) =>
                 {
-                    if (!int.TryParse(vm.Day, out var day) || !int.TryParse(vm.Month, out var month) ||
-                        !int.TryParse(vm.Year, out var year)) 
-                    { 
+                    if (string.IsNullOrEmpty(vm.Day) || string.IsNullOrEmpty(vm.Month) || string.IsNullOrEmpty(vm.Year))
+                    {
                         context.AddFailure("Date", "Enter a date of birth");
                         return;
                     }
-                    
-                    try
-                    {
-                        var achievementDate = new DateTime(year, month, day);
 
-                        if (achievementDate > SystemTime.UtcNow())
+                    if (int.TryParse(vm.Day, out var day) && int.TryParse(vm.Month, out var month) && int.TryParse(vm.Year, out var year))
+                    {
+                        var date = ValidatorExtensions.ConstructDate(vm.Day, vm.Month, vm.Year);
+
+                        if (date == null)
+                        {
+                            context.AddFailure("Date", "The date must be a real date");
+                        }
+                        else if (date > DateTime.Now.Date)
                         {
                             context.AddFailure("Date", "The date of birth must be in the past");
                         }
                     }
-                    catch (ArgumentOutOfRangeException)
+                    else
                     {
                         context.AddFailure("Date", "The date must be a real date");
                     }
                 });
-            }); 
-        }
-
-        private bool BeInValidMonthRange(string monthString)
-        {
-            var month = int.Parse(monthString);
-            return month >= 1 && month <= 12;
-        }
-
-        private bool BeInValidDayRange(string dayString)
-        {
-            var day = int.Parse(dayString);
-            return day >= 1 && day <= 31;
-        }
-
-        private bool BeANumber(string datePart)
-        {
-            return int.TryParse(datePart, out int _);
+            });
         }
     }
 }
