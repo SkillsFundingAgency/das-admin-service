6cf8b80754171c156d1f77b09892d69fea89bdeb
Karen Hanna <karen@laveryhanna.com>
2025-03-25 09:12:35 +0000

    updated batch search and framework details screens

--- src/SFA.DAS.AdminService.Web/Controllers/SearchController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/SearchController.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Http;
 using SFA.DAS.AssessorService.Api.Types.Enums;
 using System.Linq;
 using System;
+using SFA.DAS.AssessorService.Api.Types.Models.FrameworkSearch;
 
 namespace SFA.DAS.AdminService.Web.Controllers
 {
@@ -203,22 +204,30 @@ namespace SFA.DAS.AdminService.Web.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> FrameworkLearnerDetails(bool allLogs = false)
+        public async Task<IActionResult> FrameworkLearnerDetails(Guid? frameworkLearnerId = null, int? batchNumber = null, bool allLogs = false)
         {
-            var sessionModel = _sessionService.SessionFrameworkSearch;
-            if (sessionModel == null || !sessionModel.SelectedFrameworkLearnerId.HasValue)
+            FrameworkSearchSession sessionModel;
+            GetFrameworkLearnerResponse frameworkLearnerDetails; 
+            if (frameworkLearnerId.HasValue)
             {
-                return RedirectToAction("Index");
+                frameworkLearnerDetails = await _learnerDetailsApiClient.GetFrameworkLearner(frameworkLearnerId.Value, allLogs);
+            }
+            else
+            { 
+                sessionModel = _sessionService.SessionFrameworkSearch;
+                if (sessionModel == null || !sessionModel.SelectedFrameworkLearnerId.HasValue)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                frameworkLearnerDetails = await _learnerDetailsApiClient.GetFrameworkLearner(sessionModel.SelectedFrameworkLearnerId.Value, allLogs);
             }
 
-            var frameworkLearnerDetails = 
-                await _learnerDetailsApiClient.GetFrameworkLearner(sessionModel.SelectedFrameworkLearnerId.Value, allLogs);
-
-            var viewModel = _mapper.Map<FrameworkLearnerDetailsViewModel>(frameworkLearnerDetails);
+            var viewModel = _mapper.Map<FrameworkLearnerDetailsViewModel>(frameworkLearnerDetails); 
             viewModel.ShowDetails = !allLogs;
+            viewModel.BatchNumber = batchNumber; 
 
             return View(viewModel);
-
         }
 
         [HttpGet]
@@ -238,7 +247,7 @@ namespace SFA.DAS.AdminService.Web.Controllers
                 } 
                 _sessionService.ClearFrameworkSearchRequest();
             }
-            
+
             return RedirectToAction(nameof(Index));
         }
 
---
+++ src/SFA.DAS.AdminService.Web/Extensions/StaffBatchSearchResultExtensions.cs
@@ -0,0 +1,63 @@
+﻿using SFA.DAS.AssessorService.Api.Types.Models.Staff;
+using System.Collections.Generic;
+
+namespace SFA.DAS.AdminService.Web.Extensions
+{
+    public static class StaffBatchSearchResultExtensions
+    {
+        public static string DisplayUln(this StaffBatchSearchResult searchResult)
+        { 
+            return searchResult.Uln > 0 ? searchResult.Uln.ToString() : "None";  
+        }
+        public static string DisplayTrainingCourse(this StaffBatchSearchResult searchResult)
+        {
+            if (searchResult.StandardCode > 0)
+            {
+                return $"{searchResult.CertificateData.StandardName}({searchResult.StandardCode})";
+            }
+            else
+            {
+                var trainingCourse = searchResult.CertificateData.FrameworkName;
+                if (!string.IsNullOrEmpty(searchResult.CertificateData.TrainingCode))
+                {
+                    trainingCourse = trainingCourse + $"({searchResult.CertificateData.TrainingCode})";
+                }
+                return trainingCourse;
+            }
+        } 
+        public static string ViewCertificateLinkAction(this StaffBatchSearchResult searchResult)
+        {
+            if (searchResult.StandardCode > 0)
+            {
+                return "LearnerDetails";
+            }
+            else
+            {
+                return "FrameworkLearnerDetails";
+            }
+        }
+        public static Dictionary<string,string> ViewCertificateLinkRouteData(this StaffBatchSearchResult searchResult)
+        { 
+            
+            if (searchResult.StandardCode > 0)
+            {
+                return new Dictionary<string, string>()
+                {
+                    { "stdcode", searchResult.StandardCode.ToString() },
+                    { "uln", searchResult.Uln.ToString() },
+                    { "searchstring", searchResult.CertificateReference },
+                    { "batchNumber", searchResult.BatchNumber?.ToString() }
+                };
+            }
+            else
+            { 
+                return new Dictionary<string, string>()
+                {
+                    { "batchNumber", searchResult.BatchNumber?.ToString() },
+                    { "frameworkLearnerId", searchResult.FrameworkLearnerId?.ToString() },
+                };
+            } 
+        }
+
+    }
+}
