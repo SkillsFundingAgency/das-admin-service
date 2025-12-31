5137f9a72b5126d68d7ae2653206c1d0e467e78d
Deven Shah <devenshah1974@gmail.com>
2025-12-31 15:58:07 +0000

    CSP-2408 Get roatp register audit data from new endpoint

--- src/SFA.DAS.AdminService.Web/Controllers/Roatp/DownloadRoatpController.cs
+++ src/SFA.DAS.AdminService.Web/Controllers/Roatp/DownloadRoatpController.cs
@@ -11,96 +11,92 @@ using SFA.DAS.AdminService.Infrastructure.ApiClients.Roatp;
 using SFA.DAS.AdminService.Infrastructure.ApiClients.Roatp.Types;
 using SFA.DAS.AdminService.Infrastructure.ApiClients.RoatpApplication;
 using SFA.DAS.AdminService.Web.Domain;
-using SFA.DAS.AdminService.Web.Helpers;
 using SFA.DAS.AdminService.Web.Models.Roatp;
 using SFA.DAS.AdminService.Web.Services;
 using SFA.DAS.AdminService.Web.ViewModels.Roatp;
 
-namespace SFA.DAS.AdminService.Web.Controllers.Roatp
+namespace SFA.DAS.AdminService.Web.Controllers.Roatp;
+
+[Authorize(Roles = Roles.RoatpGatewayTeam)]
+[Route("")]
+public class DownloadRoatpController : Controller
 {
-    [Authorize(Roles = Roles.RoatpGatewayTeam)]
-    [Route("")]
-    public class DownloadRoatpController : Controller
+    private readonly IRoatpApiClient _apiClient;
+    private readonly ILogger<DownloadRoatpController> _logger;
+    private readonly ICsvExportService _csvExportService;
+    private readonly IRoatpApplicationApiClient _applyApiClient;
+    private readonly IMapper _mapper;
+
+    private const string CompleteRegisterWorksheetName = "Providers";
+    private const string AuditHistoryWorksheetName = "Provider history";
+    private const string ExcelFileName = "_RegisterOfApprenticeshipTrainingProviders.xlsx";
+
+    public DownloadRoatpController(IRoatpApiClient apiClient, ILogger<DownloadRoatpController> logger, ICsvExportService csvExportService, IRoatpApplicationApiClient applyApiClient, IMapper mapper)
     {
-        private readonly IRoatpApiClient _apiClient;
-        private readonly IDataTableHelper _dataTableHelper;
-        private readonly ILogger<DownloadRoatpController> _logger;
-        private readonly ICsvExportService _csvExportService;
-        private readonly IRoatpApplicationApiClient _applyApiClient;
-        private readonly IMapper _mapper;
+        ExcelPackage.License.SetNonCommercialOrganization("Department for Education");
+        _apiClient = apiClient;
+        _logger = logger;
+        _csvExportService = csvExportService;
+        _applyApiClient = applyApiClient;
+        _mapper = mapper;
+    }
 
-        private const string CompleteRegisterWorksheetName = "Providers";
-        private const string AuditHistoryWorksheetName = "Provider history";
-        private const string ExcelFileName = "_RegisterOfApprenticeshipTrainingProviders.xlsx";
-
-        public DownloadRoatpController(IRoatpApiClient apiClient, IDataTableHelper dataTableHelper,
-            ILogger<DownloadRoatpController> logger, ICsvExportService csvExportService, IRoatpApplicationApiClient applyApiClient, IMapper mapper)
+    [Route("download-register")]
+    public async Task<IActionResult> Download()
+    {
+        using (var package = new ExcelPackage())
         {
-            ExcelPackage.License.SetNonCommercialOrganization("Department for Education");
-            _apiClient = apiClient;
-            _dataTableHelper = dataTableHelper;
-            _logger = logger;
-            _csvExportService = csvExportService;
-            _applyApiClient = applyApiClient;
-            _mapper = mapper;
-        }
-
-        [Route("download-register")]
-        public async Task<IActionResult> Download()
-        {
-            using (var package = new ExcelPackage())
+            var completeRegisterWorkSheet = package.Workbook.Worksheets.Add(CompleteRegisterWorksheetName);
+            GetAllOrganisationsResponse registerData = await _apiClient.GetCompleteRegister();
+            if (registerData?.Organisations.Count > 0)
             {
-                var completeRegisterWorkSheet = package.Workbook.Worksheets.Add(CompleteRegisterWorksheetName);
-                GetAllOrganisationsResponse registerData = await _apiClient.GetCompleteRegister();
-                if (registerData != null && registerData.Organisations.Any())
-                {
-                    IEnumerable<ProviderRegisterModel> items = registerData.Organisations.Select(r => (ProviderRegisterModel)r).OrderBy(r => r.LegalName);
-                    completeRegisterWorkSheet.Cells.LoadFromCollection(items, true);
-                }
-                else
-                {
-                    _logger.LogError("Unable to retrieve register data from RoATP API");
-                }
-
-                var auditHistoryWorksheet = package.Workbook.Worksheets.Add(AuditHistoryWorksheetName);
-                var auditHistoryData = await _apiClient.GetAuditHistory();
-                if (auditHistoryData != null && auditHistoryData.Any())
-                {
-                    auditHistoryWorksheet.Cells.LoadFromDataTable(_dataTableHelper.ToDataTable(auditHistoryData), true);
-                }
-                else
-                {
-                    _logger.LogError("Unable to retrieve audit history data from RoATP API");
-                }
-                byte[] fileContent = await package.GetAsByteArrayAsync();
-                return File(fileContent, "application/excel", $"{DateTime.Now.ToString("yyyyMMdd")}{ExcelFileName}");
+                IEnumerable<ProviderRegisterModel> items = registerData.Organisations.Select(r => (ProviderRegisterModel)r).OrderBy(r => r.LegalName);
+                completeRegisterWorkSheet.Cells.LoadFromCollection(items, true);
             }
-        }
+            else
+            {
+                _logger.LogError("Unable to retrieve register data from RoATP API");
+            }
 
-        [Route("application-download")]
-        public IActionResult ApplicationDownload()
+            var auditHistoryWorksheet = package.Workbook.Worksheets.Add(AuditHistoryWorksheetName);
+            var auditHistoryData = await _apiClient.GetAuditHistory();
+            if (auditHistoryData != null && auditHistoryData.AuditRecords.Any())
+            {
+                IEnumerable<OrganisationAuditModel> items = auditHistoryData.AuditRecords.Select(r => (OrganisationAuditModel)r);
+                auditHistoryWorksheet.Cells.LoadFromCollection(items, true);
+            }
+            else
+            {
+                _logger.LogError("Unable to retrieve audit history data from RoATP API");
+            }
+            byte[] fileContent = await package.GetAsByteArrayAsync();
+            return File(fileContent, "application/excel", $"{DateTime.Now.ToString("yyyyMMdd")}{ExcelFileName}");
+        }
+    }
+
+    [Route("application-download")]
+    public IActionResult ApplicationDownload()
+    {
+        return View("~/Views/Roatp/ApplicationDownload.cshtml");
+    }
+
+    [Route("application-download/download")]
+    public async Task<IActionResult> ApplicationDownloadCsv(ApplicationDownloadViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
         {
             return View("~/Views/Roatp/ApplicationDownload.cshtml");
         }
 
-        [Route("application-download/download")]
-        public async Task<IActionResult> ApplicationDownloadCsv(ApplicationDownloadViewModel viewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View("~/Views/Roatp/ApplicationDownload.cshtml");
-            }
+        var applications = await _applyApiClient.GetApplicationOversightDetailsForDownload(viewModel.FromDate.Value, viewModel.ToDate.Value);
 
-            var applications = await _applyApiClient.GetApplicationOversightDetailsForDownload(viewModel.FromDate.Value, viewModel.ToDate.Value);
+        var exportModel = _mapper.Map<List<RoatpOversightOutcomeExportViewModel>>(applications);
 
-            var exportModel = _mapper.Map<List<RoatpOversightOutcomeExportViewModel>>(applications);
+        var bytearray = _csvExportService
+            .WriteCsvToByteArray<RoatpOversightOutcomeExportViewModel, RoatpOversightOutcomeExportCsvMap>(exportModel);
 
-            var bytearray = _csvExportService
-                .WriteCsvToByteArray<RoatpOversightOutcomeExportViewModel, RoatpOversightOutcomeExportCsvMap>(exportModel);
+        var fileName = $"outcomes_{viewModel.FromDate:ddMMyy}_to_{viewModel.ToDate.Value:ddMMyy}.csv";
 
-            var fileName = $"outcomes_{viewModel.FromDate:ddMMyy}_to_{viewModel.ToDate.Value:ddMMyy}.csv";
-
-            return File(bytearray, "text/csv", fileName);
-        }
+        return File(bytearray, "text/csv", fileName);
     }
 }
